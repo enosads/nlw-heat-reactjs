769db0d42004cdc87ad7fc2e121b9a022d14b5c1
Enos Andrade <enosads@gmail.com>
2021-10-20 17:21:56 -0300

    stage 02

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,18 @@
+import styles from "./styles.module.scss";
+import {VscGithubInverted} from "react-icons/vsc"
+import {useAuth} from "../../contexts/auth";
+
+
+export function LoginBox () {
+  const {signInUrl} = useAuth();
+
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={signInUrl} className={styles.signInWithGithub}>
+        <VscGithubInverted size="24"/>
+        Entrar com o GitHub
+      </a>
+    </div>
+  );
+}
